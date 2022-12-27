eb6912fb021cebc11bb97c3d147104bc4f47a6b9
Simar Pal Singh <simarpalsingh@Simars-MBP-2.modem>
2022-12-27 22:46:14 +1100

    more updates

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 // create all the handlers for our routes inside here
 export const getPosts = async (req, res) => {
@@ -22,3 +23,39 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body; // this is being sent from the front end.
+
+  // check if the id specified is actually a mongoose id
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true }); // {new: true } to receive the updated version of the post
+
+  res.json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body; // this is being sent from the front end.
+
+  // check if the id specified is actually a mongoose id
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+  await PostMessage.findByIdAndRemove(_id);
+
+  res.json({ message: 'Post deleted successfully' })
+}
+
+export const likePost = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id')
+  // find the post we looking for 
+  const post = await PostMessage.findById(id)
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true }) //passing updates to it as the second parameter
+
+  res.json(updatedPost)
+}
--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from "body-parser";
 import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 import postRoutes from './routes/post.js'
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config()
 
 // setting up body parser so we can properly send our request
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes) // this needs to  be after app.cors()
 
-const CONNECTION_URL = "mongodb+srv://mern:mongodb@mernapp01.wrf2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT= process.env.PORT || 4000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
